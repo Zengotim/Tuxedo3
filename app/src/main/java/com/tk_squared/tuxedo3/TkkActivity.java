3b3d74bee5b00138e79a4b9ba37b5f0fa59ff8fd
Zengotim <zengotim@yahoo.com>
2018-11-28 18:09:14 -0500

    Changed ad provider and updated to newest Google Play Store requirements.

--- app/src/main/java/com/tk_squared/tuxedo3/TkkActivity.java
+++ app/src/main/java/com/tk_squared/tuxedo3/TkkActivity.java
@@ -20,6 +20,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -32,6 +33,12 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.support.v7.widget.ShareActionProvider;
 import android.widget.TextView;
+
+import com.smaato.soma.AdDownloaderInterface;
+import com.smaato.soma.AdListenerInterface;
+import com.smaato.soma.BannerView;
+import com.smaato.soma.ReceivedBannerInterface;
+import com.smaato.soma.bannerutilities.constant.BannerStatus;
 
 
 /**
@@ -81,7 +88,16 @@ public class TkkActivity extends AppCompatActivity
         }
 
         adSupport.setupAdSupport();
-
+        /*BannerView bannerView = findViewById(R.id.ad_container);
+        bannerView.addAdListener(new AdListenerInterface() {
+            @Override
+            public void onReceiveAd(AdDownloaderInterface adDownloaderInterface, ReceivedBannerInterface receivedBannerInterface) {
+                if (receivedBannerInterface.getStatus() == BannerStatus.ERROR) {
+                    Log.i("BannerAd", "" + receivedBannerInterface.getErrorCode() + " : " + receivedBannerInterface.getErrorMessage());
+                }
+            }
+        });
+        */
         //Set up the headphone jack listener
         musicIntentReceiver = new MusicIntentReceiver(this);
 
@@ -291,6 +307,7 @@ public class TkkActivity extends AppCompatActivity
     //Callback method for tkkDataMod.Callbacks
     @Override
     public void onDataLoaded() {
+        Log.i("ListData", Integer.toString(tuxData.getStations().size()));
         progBar.setVisibility(View.GONE);
         displayListView();
         adSupport.loadInterstitial();
@@ -353,5 +370,5 @@ public class TkkActivity extends AppCompatActivity
                     Context.BIND_AUTO_CREATE);
         }
     }
-
+    //endregion
 }
